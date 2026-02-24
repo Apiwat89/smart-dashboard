adfee98f6c4d9e39be0b107eec8e85ca2ac2a5e2
Apiwat89 <apiwat89123@gmail.com>
2026-02-25 00:15:01 +0700

    save

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://smart-dashboard-rosy.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'] 
